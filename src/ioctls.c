628b6b646b4ac1f5d4de06c328601d852736f5d0
rmcmahon <bob.mcmahon@broadcom.com>
2018-04-06 15:38:59 -0700

    use timespec for gps and ref sync

--- src/ioctls.c
+++ src/ioctls.c
@@ -213,13 +213,11 @@ void tsfgps_sync (tsftv_t *tsf,  struct gpsref_sync_t *t, thread_Settings *agent
 	}
 	tsf->gpsref_sync.gps_ts.tv_sec  = t1.tv_sec - SHIFTSECONDS;
 	tsf->gpsref_sync.gps_ts.tv_nsec  = t1.tv_nsec;
-	tsf->raw = 0xFFFFFFFF;
     } else {
 	tsf->gpsref_sync.gps_ts.tv_sec  = t->gps_ts.tv_sec;
 	tsf->gpsref_sync.gps_ts.tv_nsec  = t->gps_ts.tv_nsec;
 	tsf->gpsref_sync.ref_ts.tv_sec  = t->ref_ts.tv_sec;
 	tsf->gpsref_sync.ref_ts.tv_nsec  = t->ref_ts.tv_nsec;
-	tsf->raw = 0xFFFFFFFF;
     }
     tsf->synced = 1;
 }
